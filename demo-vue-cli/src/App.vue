88b0412ac4889bb791e2918e1e25f70d1abc1f18
Noel Vaca <noel.vm.2501@gmail.com>
2021-10-11 19:44:15 -0400

    Demo Vue Cli - Clase 11/10/2021

---
+++ demo-vue-cli/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div> -->
+  <router-view />
+</template>
+
+<style>
+body {
+  background-color: #f2f2f2;
+  margin: 0px;
+  font-family: tahoma;
+  color: #282828;
+}
+</style>
---
+++ demo-vue-cli/src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="nav-bar">
+    <div class="cart">Carro {{ state.cart.length }}</div>
+    <ProductDisplay :premium="state.premium" @add-to-cart="updateCart" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ProductDisplay from "@/components/ProductDisplay.vue";
+import { reactive } from "vue";
+
+export default {
+  name: "Home",
+  components: {
+    ProductDisplay,
+  },
+  setup() {
+    const state = reactive({
+      cart: [],
+      premium: true,
+    });
+
+    const updateCart = (id) => {
+      state.cart.push(id);
+    };
+
+    return {
+      state,
+      updateCart,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.nav-bar {
+  background: linear-gradient(-90deg, #84cf6a, #16c0b0);
+  height: 60px;
+  margin-bottom: 25px;
+  -webkit-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
+  -moz-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.57);
+}
+.cart {
+  margin: 5rem 6rem;
+  float: right;
+  border: 1px solid #d8d8d8;
+  padding: 10px 30px;
+  background-color: white;
+  -webkit-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
+  -moz-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
+  box-shadow: 2px 15px -12px rgba(0, 0, 0, 0.57);
+}
+</style>
