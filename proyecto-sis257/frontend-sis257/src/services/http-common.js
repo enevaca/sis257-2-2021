74501c3fb34db19cba010e00728d2d6a23237b39
Noel Vaca <noel.vm.2501@gmail.com>
2021-12-06 22:06:43 -0400

    Demo Heroku backend y frontend

--- proyecto-sis257/frontend-sis257/src/services/http-common.js
+++ proyecto-sis257/frontend-sis257/src/services/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 var token = localStorage.getItem("token");
 
 export default axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://backend-sis257.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
     Authorization: "Bearer " + token,
